c24cf81d7b4a6a1c675941aadcb0c04e35e088cb
Roman Freiberg <freiberg@Mac.fritz.box>
2025-01-24 19:27:23 +0100

    add neovim setup from omen

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,8 @@
+require("obsidian").setup({
+  workspaces = {
+    {
+      name = "Notes",
+      path = "/Users/freiberg/Library/Mobile Documents/iCloud~md~obsidian/Documents/rf2note",
+    },
+  },
+})
