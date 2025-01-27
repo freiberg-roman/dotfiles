85f2545fc8c18dfd333028366994f2084196da00
Roman Freiberg <freiberg@Mac.fritz.box>
2025-01-27 20:01:47 +0100

    remove unnecessary lsps and add obsidian settings

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,8 +1,27 @@
 require("obsidian").setup({
   workspaces = {
     {
-      name = "Notes",
-      path = "/Users/freiberg/Library/Mobile Documents/iCloud~md~obsidian/Documents/rf2note",
+      name = "notes",
+      path = "/Users/freiberg/Library/Mobile Documents/iCloud~md~obsidian/Documents/rf2note"
     },
   },
+  completion = {
+    nvim_cpm = true,
+    min_chars = 2,
+    new_note_location = "0. Inbox",
+  },
+  mappings = {
+    ["<leader>of"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+  },
+  templates = {
+    folder = "Templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%H:%M",
+  },
 })
+
