f974fa31c6e2db70e908085fa4eb01d55e5ca2cb
Roman Freiberg <roman.sascha@googlemail.com>
2025-02-18 16:36:02 +0100

    add down and up movement for the stack frame during debugging

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -24,3 +24,5 @@ vim.api.nvim_set_keymap("n", "<leader>ds", ":lua require('dap').step_over()<CR>"
 vim.api.nvim_set_keymap("n", "<leader>di", ":lua require('dap').step_into()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>do", ":lua require('dap').step_out()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>du", ":lua require('dapui').toggle()<CR>", { noremap = true, silent = true })
++vim.api.nvim_set_keymap("n", "<leader>dp", ":lua require('dap').up()<CR>", { noremap = true, silent = true })
++vim.api.nvim_set_keymap("n", "<leader>dn", ":lua require('dap').down()<CR>", { noremap = true, silent = true })
