251f924b19a2e6f60cb6e26ca0626404b4062b65
Roman Freiberg <roman.sascha@googlemail.com>
2025-03-05 21:04:07 +0100

    back to copilot

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,17 +5,18 @@ require('lualine').setup {
     section_separators = '',
   },
   sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
     lualine_x = {
       {
         require("noice").api.status.command.get,
         cond = require("noice").api.status.command.has,
         color = { fg = "#ff9e64" },
       },
-      lualine_y = {},
-      lualine_z = {'location'},
     },
-    lualine_a = {},
-    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {'location'},
   }
 }
 
