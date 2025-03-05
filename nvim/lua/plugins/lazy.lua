352764b6c57d3a3df9a11547b7f4d5c059b4bc15
Roman Freiberg <roman.sascha@googlemail.com>
2025-03-05 21:04:07 +0100

    back to copilot

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -391,7 +391,29 @@ require('lazy').setup({
     opts = {
     }
   },
-  { 'huggingface/llm.nvim', },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = "<Tab>", -- handled by nvim-cmp / blink.cmp
+          next = "<leader>la",
+          prev = "<leader>ld",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
   {
     "mfussenegger/nvim-dap-python",
     keys = {
