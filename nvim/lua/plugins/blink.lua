c72a2d3583c3e4e4003dc39c377a48e6d652907b
Roman Freiberg <roman.sascha@googlemail.com>
2025-03-07 16:07:29 +0100

    add copilot support to blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,2 +1,13 @@
- require('blink.cmp').setup({})
-
+require('blink.cmp').setup({
+  sources = {
+    default = { "copilot", "lsp", "path", "snippets", "buffer" },
+    providers = {
+      copilot = {
+        name = "copilot",
+        module = "blink-cmp-copilot",
+        score_offset = 100,
+        async = true,
+      },
+    },
+  }
+})
