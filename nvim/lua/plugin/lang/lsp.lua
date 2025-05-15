b14708c70377889f03ba41808aed57d9ffc7b1e5
Roman Freiberg <roman.sascha@googlemail.com>
2025-05-15 22:44:26 +0200

    update formatting

--- nvim/lua/plugin/lang/lsp.lua
+++ nvim/lua/plugin/lang/lsp.lua
@@ -128,23 +128,6 @@ return {
           })
         end,
       })
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local c = vim.lsp.get_client_by_id(args.data.client_id)
-          if not c then return end
-
-          if c.supports_method('textDocument/formatting') then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
-              end,
-            })
-          end
-        end,
-      })
     end,
   },
 }
