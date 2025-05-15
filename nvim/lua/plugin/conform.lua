6d7019e56074a6948b74887ab3a4c00a0295bbed
Roman Freiberg <roman.sascha@googlemail.com>
2025-05-15 22:44:26 +0200

    update formatting

---
+++ nvim/lua/plugin/conform.lua
@@ -0,0 +1,18 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {},
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black", lsp_format = "fallback" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      format_on_save = {
+        timeout_ms = 5000,
+        lsp_format = "fallback",
+      },
+    })
+  end,
+}
