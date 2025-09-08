c6c2c5d2b25e8367afbf2441d95b15a0ad50da0e
Roman Freiberg <roman.sascha@googlemail.com>
2025-09-08 21:25:31 +0200

    add mcp hub to config

---
+++ nvim/lua/plugin/mcp.lua
@@ -0,0 +1,20 @@
+return {
+  "ravitemer/mcphub.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+  config = function()
+    require("mcphub").setup({
+      config = vim.fn.expand("~/.config/nvim/mcp/servers.json"),
+      extensions = {
+        copilotchat = {
+          enabled = true,
+          convert_tools_to_functions = true,     -- Convert MCP tools to CopilotChat functions
+          convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
+          add_mcp_prefix = false,                -- Add "mcp_" prefix to function names
+        }
+      }
+    })
+  end
+}
