7069a6e64a94df6815e837cadf27cfe089eefcc5
Roman Freiberg <roman.sascha@googlemail.com>
2026-02-12 20:13:02 +0100

    make ty single source of truth for goto

--- nvim/lua/plugin/lang/lsp/python.lua
+++ nvim/lua/plugin/lang/lsp/python.lua
@@ -18,6 +18,7 @@ function M.setup(capabilities)
   vim.lsp.config('ruff', {
     on_attach = function(client, _)
       client.server_capabilities.hoverProvider = false
+      client.server_capabilities.definitionProvider = false
     end,
     capabilities = capabilities,
     init_options = {
