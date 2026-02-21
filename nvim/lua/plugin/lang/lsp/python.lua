64666f68116cf5a37307028dcf914b470c66e310
Roman Freiberg <roman.sascha@googlemail.com>
2026-02-21 11:26:36 +0100

    try better ty settings

--- nvim/lua/plugin/lang/lsp/python.lua
+++ nvim/lua/plugin/lang/lsp/python.lua
@@ -1,17 +1,16 @@
 local M = {}
 
 function M.setup(capabilities)
+  local util = require('lspconfig.util')
+
   vim.lsp.config('ty', {
     on_attach = function(client, _)
       client.server_capabilities.documentFormattingProvider = false
     end,
     capabilities = capabilities,
-    settings = {
-      ty = {
-        analysis = {
-        },
-      },
-    },
+    root_dir = function(fname)
+      return util.root_pattern('.git', 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt')(fname) or util.path.dirname(fname)
+    end,
   })
   vim.lsp.enable('ty')
 
