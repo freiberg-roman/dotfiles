9d127ab2ca7c8f59d0ad4fe53f39a60eecba8365
Roman Freiberg <roman.sascha@googlemail.com>
2026-05-01 23:06:59 +0200

    add repository 99 to neovim config (raw)

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,31 @@ require('keymaps')
 require('pm')
 require('options')
 require('misc')
+
+vim.opt.rtp:append(vim.fn.stdpath("config") .. "/99")
+local _99 = require('99')
+
+local cwd = vim.uv.cwd()
+local basename = vim.fs.basename(cwd)
+_99.setup({
+    provider = _99.Providers.OpenCodeProvider,
+    logger = {
+        level = _99.DEBUG,
+        path = "/tmp/" .. basename .. ".99.debug",
+        print_on_error = true,
+    },
+    tmp_dir = "./tmp",
+
+})
+
+vim.keymap.set("v", "<leader>9v", function()
+    _99.visual()
+end)
+
+vim.keymap.set("n", "<leader>9x", function()
+    _99.stop_all_requests()
+end)
+
+vim.keymap.set("n", "<leader>9s", function()
+    _99.search()
+end)
